b2a23d063efbaae7f59ee59c98d019347de6921c
John Doe <patrick.gould94@gmail.com>
2019-02-15 23:30:06 -0500

    Finished game logic, added basics for end game modal *Still in progress*

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import Nav from "./components/Nav";
 import images from "./images";
 import CardWrapper from "./components/CardWrapper";
+import SimpleModalWrapped from "./components/SimpleModalWrapped";
 
 class App extends Component {
   state = {
@@ -11,28 +12,46 @@ class App extends Component {
     images: images,
   };
 
+  // Fisher Yates Shuffle Algorithm in ES6 *Credit: https://stackoverflow.com/questions/6274339/how-can-i-shuffle-an-array
+  shuffleImages = images => {
+    for (let i = images.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [images[i], images[j]] = [images[j], images[i]];
+    }
+    return images;
+  };
+
   clickHandler = id => {
-    console.log("This card was clicked and has an ID of " + id);
-    let cards = this.state.images;
-    let newScore = this.state.score + 1;
+    const cards = this.state.images;
+    const newScore = this.state.score + 1;
 
     // If the clicked card hasn't been clicked update the state
-    console.log(cards[id]);
     if (!cards[id].clicked) {
       // Set the cards state to true
       cards[id].clicked = true;
-      // Update the the state with the new cards and increase the score
+      // Update the the state with the shuffled cards and increase the score
       this.setState({
         score: newScore,
-        images: cards,
+        images: this.shuffleImages(cards),
       });
+      // If the current score is greater than the highscore update the current highscore
       if (this.state.score > this.state.highScore) {
         this.setState({
           highScore: newScore,
         });
       }
     } else {
-      console.log("This card has already been clicked.");
+      // If the clicked card has already been clicked
+      // Map over the cards and set all the clicked states to false
+      const resetCards = cards.map(card => {
+        card.clicked = false;
+        return card;
+      });
+      // Update the state with the reset cards and a score of 0
+      this.setState({
+        score: 0,
+        images: resetCards,
+      });
     }
   };
   render() {
@@ -43,6 +62,7 @@ class App extends Component {
           images={this.state.images}
           clickHandler={this.clickHandler}
         />
+        <SimpleModalWrapped ref="modal" />
       </div>
     );
   }
